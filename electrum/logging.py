0f11305789e8364f4a896253f8ff76087f756f95
Andrey <andrey@zcoin.io>
2019-06-24 08:35:56 +0400

    Removed debug code

--- electrum/logging.py
+++ electrum/logging.py
@@ -60,12 +60,12 @@ def _shorten_name_of_logrecord(record: logging.LogRecord) -> logging.LogRecord:
 
 # enable logs universally (including for other libraries)
 root_logger = logging.getLogger()
-root_logger.setLevel(logging.DEBUG)
+root_logger.setLevel(logging.WARNING)
 
 # log to stderr; by default only WARNING and higher
 console_stderr_handler = logging.StreamHandler(sys.stderr)
 console_stderr_handler.setFormatter(console_formatter)
-console_stderr_handler.setLevel(logging.DEBUG)
+console_stderr_handler.setLevel(logging.WARNING)
 root_logger.addHandler(console_stderr_handler)
 
 # creates a logger specifically for electrum library
